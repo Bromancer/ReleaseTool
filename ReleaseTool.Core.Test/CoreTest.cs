0c77f5a66c9970fe52fd5dd5dd9e5ded7cafa8c5
Michiel Hegemans <hegemansmichiel@gmail.com>
2015-04-10 16:39:51 +0200

    Project commit

---
+++ ReleaseTool.Core.Test/CoreTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReleaseTool.Core.Test
+{
+    [TestClass]
+    public class CoreTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
